7a198dceb0f8da1a3935a7d32c0b90fa9c35c7d6
VeselinGerenski <veselingerenski@gmail.com>
2025-11-26 12:31:15 +0200

    Add Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
+import Header from "./components/header/Header.jsx"
+
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header />
     </>
   )
 }
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router";
+
+export default function Header() {
+    return (
+
+        <header className="absolute inset-x-0 top-0 z-50 text-white bg-gradient-to-b from-black/60 via-black/10 to-transparent">
+
+            {/* Full width nav bar */}
+            <nav className="w-full flex items-center justify-between px-10 py-3 -mt-4 font-['Playfair_Display']">
+
+                {/* LEFT – LOGO */}
+                <Link
+                    to="/" className="flex items-center group">
+                    <img
+                        src="/images/logo.png"
+                        alt="logo"
+                        className="w-30 h-auto drop-shadow-xl ink-link hover-scale group-hover:drop-shadow-[0_0_25px_rgba(217,119,6,0.55)]"
+                    />
+                </Link>
+
+                {/* NAVIGATION */}
+                <div className="absolute left-[55%] -translate-x-1/2">
+
+
+                    <ul className="hidden lg:flex gap-16 text-2xl font-semibold tracking-wide">
+                        <li>
+                            <Link to="/" className="ink-link inline-block text-3xl  hover-scale">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/catalog" className="ink-link inline-block text-3xl hover-scale">Catalog</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/create" className="ink-link inline-block text-3xl hover-scale">Add City</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/register" className="ink-link inline-block text-3xl hover-scale">Register</Link>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div className="hidden lg:block text-3xl font-semibold tracking-wide ml-6">
+                    <Link to="/login" className="ink-link inline-block flex items-center gap-2 hover-scale">
+                        Login →
+                    </Link>
+                </div>
+
+            </nav>
+        </header>
+    );
+};
