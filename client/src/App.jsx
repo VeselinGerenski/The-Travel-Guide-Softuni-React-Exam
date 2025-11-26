8ad5c8d5b6169fc55abd5e00831574a703b31472
VeselinGerenski <veselingerenski@gmail.com>
2025-11-26 12:41:24 +0200

    Add CSS and image resources

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import Header from "./components/header/Header.jsx"
 function App() {
 
   return (
-    <>
+    <div className="default-bg">
       <Header />
 
 
       <Footer />
-    </>
+
+    </div>
+
   )
 }
 
