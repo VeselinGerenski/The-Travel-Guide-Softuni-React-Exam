a5245bc9d7f125db8571451f76e258cb48ffef0f
VeselinGerenski <veselingerenski@gmail.com>
2025-11-27 17:23:22 +0200

    Create useControlledForm hook

---
+++ client/src/hooks/useControlledForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export default function useControlledForm(initialValues, onSubmit) {
+    const [values, setValues] = useState(initialValues)
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        await onSubmit(values)
+
+        setValues(initialValues)
+    }
+    // return { values, changeHandler, submitHandler }
+    return {
+        values,
+        changeHandler,
+        submitHandler,
+    }
+}
