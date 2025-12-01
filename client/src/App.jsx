3562d48a7f71623d2e8c3adacc1307938ec57865
VeselinGerenski <veselingerenski@gmail.com>
2025-12-01 11:21:26 +0200

    Add user provider to main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,40 +10,34 @@ import DetailsCity from "./components/details-city/DetailsCity.jsx"
 import EditCity from "./components/edit-city/EditCity.jsx"
 import Page404 from "./components/page-404/Page404.jsx"
 import useBackground from "./hooks/useBackground.js"
-import UserContext from "./contexts/UserContext.js"
-import useAuth from "./hooks/useAuth.js"
 import GuestGuard from "./components/guards/GuestGuard.jsx"
 import AuthGuard from "./components/guards/AuthGuard.jsx"
 
 function App() {
-  const auth = useAuth()
 
   return (
     <div className={useBackground()}>
-      <UserContext.Provider value={auth}>
-        <Header />
+      <Header />
 
-        <Routes >
-          <Route path="/" element={<Home />} />
-          <Route path="/Catalog" element={<Catalog />} />
-          <Route path="/details/:cityId" element={<DetailsCity />} />
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/Catalog" element={<Catalog />} />
+        <Route path="/details/:cityId" element={<DetailsCity />} />
 
-          <Route element={<GuestGuard />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Route>
+        <Route element={<GuestGuard />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
 
-          <Route element={<AuthGuard />}>
-            <Route path="/create" element={<CreateCity />} />
-            <Route path="/edit/:cityId" element={<EditCity />} />
-          </Route>
+        <Route element={<AuthGuard />}>
+          <Route path="/create" element={<CreateCity />} />
+          <Route path="/edit/:cityId" element={<EditCity />} />
+        </Route>
 
-          <Route path="*" element={<Page404 />} />
-        </Routes>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
 
-        <Footer />
-
-      </UserContext.Provider>
+      <Footer />
     </div>
 
   )
